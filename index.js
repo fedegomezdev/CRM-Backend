9c8f69ab5c46ef93cd3eca0cd4a0020c087d9e8c
fede <fedegomezdev@gmail.com>
2019-10-17 18:54:42 -0300

    agregando cors

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
 
+//cors permite que un cliente se conecte a otro servidor para el intercambio ded recursos
+const cors = require('cors');
+
+
 require('./database');
 
 app.use(express.json());
+
+app.use(cors());
+
 app.use('/' , routes());
 
-app.listen(3000);
+app.listen(4000);
